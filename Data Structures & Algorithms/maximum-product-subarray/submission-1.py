79e397b8a1926446d9af281b16e71d95c01136b5
Yash Kerkar <ykerkar61@gmail.com>
2026-05-21 16:25:10 +0530

    Add: maximum-product-subarray - submission-1

---
+++ Data Structures & Algorithms/maximum-product-subarray/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        
+        if not nums:
+            return 0
+
+        global_max , current_max , current_min = nums[0] , nums[0] , nums[0]
+
+        for i in range(1 , len(nums)):
+
+            options = (nums[i] , current_max * nums[i] , current_min * nums[i])
+            current_max = max(options)
+            current_min = min(options)
+            global_max = max(global_max , current_max)
+
+        return global_max
