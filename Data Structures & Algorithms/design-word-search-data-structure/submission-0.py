c1af2c4515197e8f0b5557c34616e1ad0f95a53d
Yash Kerkar <ykerkar61@gmail.com>
2026-06-06 16:03:29 +0530

    Add: design-word-search-data-structure - submission-0

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-0.py
@@ -0,0 +1,34 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end_of_word = False
+
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        current = self.root
+        for ch in word:
+            if ch not in current.children:
+                current.children[ch] = TrieNode()
+            current = current.children[ch]
+        current.is_end_of_word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(idx , root):
+            current = root
+            for i in range(idx , len(word)):
+                ch = word[i]
+                if ch == '.':
+                    for child in current.children.values():
+                        if dfs(i + 1 , child):
+                            return True
+                    return False
+                else:
+                    if ch not in current.children:
+                        return False
+                    current = current.children[ch]
+            return current.is_end_of_word
+        return dfs(0 , self.root)
