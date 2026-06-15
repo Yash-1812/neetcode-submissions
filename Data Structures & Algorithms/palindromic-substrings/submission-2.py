7a79a46cd8ba881299556c669ab8c1a7e291ce74
Yash Kerkar <ykerkar61@gmail.com>
2026-06-15 23:51:25 +0530

    Add: palindromic-substrings - submission-2

---
+++ Data Structures & Algorithms/palindromic-substrings/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        
+        total = 0
+        def expand_from_center(left , right):
+            count = 0
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+                count += 1
+            return count
+
+        for i in range(len(s)):
+            total += expand_from_center(i , i)
+            total += expand_from_center(i , i + 1)
+        
+        return total
