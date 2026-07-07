68bbd4f840313c09aeb6222773b6ee63ab7a6e1e
Yash Kerkar <ykerkar61@gmail.com>
2026-07-07 23:04:54 +0530

    Add: subarray-sum-equals-k - submission-3

---
+++ Data Structures & Algorithms/subarray-sum-equals-k/submission-3.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        curr_sum = 0
+        prefix_sum = collections.defaultdict(int)
+        prefix_sum[0] += 1
+        for num in nums:
+            curr_sum += num
+
+            if (curr_sum - k) in prefix_sum:
+                count += prefix_sum[curr_sum - k]
+            prefix_sum[curr_sum] += 1
+        return count
